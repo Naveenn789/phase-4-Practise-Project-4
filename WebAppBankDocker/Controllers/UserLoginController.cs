ad1343bafb2f89afbb8e15c3a2c25a2caa2dc2e4
Naveenn789 <naveengalla77@gmail.com>
2024-01-18 13:32:33 +0530

    Docker

---
+++ WebAppBankDocker/Controllers/UserLoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppBankDocker.Models;
+
+namespace WebAppBankDocker.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        readonly List<UserLogin> users = new List<UserLogin>()
+        {
+            new UserLogin() {UserName="Sam" ,Password="Sam@123"},
+            new UserLogin() {UserName="Ram" ,Password="Ram@123"},
+            new UserLogin() {UserName="Sameer" ,Password="Sameer@123"},
+            new UserLogin() {UserName="Amir" ,Password="Amir@123"},
+        };
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.UserName == username && user.Password == password)
+                {
+                    return RedirectToAction("Dashboard");
+                }
+                ViewBag.ErrorMessage = "Invalid username or password";
+            }
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
